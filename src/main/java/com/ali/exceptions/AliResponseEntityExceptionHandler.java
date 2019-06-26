f799486f203a0c9f6acdf4d83ad74bdda83f4899
Alireza <alireza.akbari@spirent.com>
2019-06-26 17:36:05 -0400

    Exception handling

---
+++ src/main/java/com/ali/exceptions/AliResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.ali.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * Created by alireza on 6/26/19.
+ */
+
+@ControllerAdvice
+@RestController
+public class AliResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(CourseNotFoundException.class)
+    public final ResponseEntity<Object> handleCourseNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/ali/exceptions/CourseNotFoundException.java
+++ src/main/java/com/ali/exceptions/CourseNotFoundException.java
@@ -1,8 +1,13 @@
 package com.ali.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by alireza on 6/26/19.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND) //not mandatory if there is a generic exception handling for all resources:
+                                        // like ExceptionResponse.java and AliResponseEntityExceptionHandler.java
 public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(String s) {
         super(s);
