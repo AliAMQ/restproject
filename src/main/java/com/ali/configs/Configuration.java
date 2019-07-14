23516d927216a0bc53fd4bc7a5dc58f181e7203b
AliAMQ <alireza.amq@gmail.com>
2019-07-14 09:26:27 -0400

    fix properties

--- src/main/java/com/ali/configs/Configuration.java
+++ src/main/java/com/ali/configs/Configuration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("restproject")
+@ConfigurationProperties("rest-project")
 public class Configuration {
 
     private int max;
